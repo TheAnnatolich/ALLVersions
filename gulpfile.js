6eaef804091a9ebdd80d504d76716b74a04fdf61
TheAnnatolich <lotvin.1999@ukr.net>
2020-05-04 18:46:31 +0300

    add_gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('hello', function (done) {
+    console.log('Hi');
+    done();
+})
+
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});

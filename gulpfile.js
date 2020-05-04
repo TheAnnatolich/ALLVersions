831fb156312ef4f3ec3730588e3e39ef9cca5672
TheAnnatolich <lotvin.1999@ukr.net>
2020-05-04 20:25:19 +0300

    добавили минимизацию для CSS

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,12 @@ gulp.task("browser-sync", function () {
   });
   gulp.watch("./*.html").on("change", browserSync.reload);
 });
+
+var minifycss = require("gulp-minify-css");
+
+gulp.task("css", function () {
+  return gulp
+    .src("src/css/*.css")
+    .pipe(minifycss())
+    .pipe(gulp.dest("dist/css"));
+});
